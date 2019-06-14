103b6542563c46803ac543275716e0c1eced58bf
Federico <fserenelli@gmail.com>
2019-06-14 13:47:11 -0300

    js reservas, validations

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@
 
 	document.addEventListener('DOMContentLoaded', function(){
 
+		// VARIABLES //
+
 		// Campos datos usuario
 
 		var nombre = document.querySelector('#nombre');
@@ -22,7 +24,8 @@
 		var calcular = document.querySelector('#calcular');
 		var errorDiv = document.querySelector('#error');
 		var botonRegistro = document.querySelector('#btnRegistro');
-		var resulado = document.querySelector('#lista-productos');
+		var lista_productos = document.querySelector('#lista-productos');
+		var suma = document.querySelector('#suma-total');
 
 		// Extras
 
@@ -30,8 +33,21 @@
 		var camisas = document.querySelector('#camisa_evento');
 
 
+		// FUNCIONES //
 
+
+		// Calcular totales
 		calcular.addEventListener('click', calcularMontos);
+
+		// Mostrar días de acuerdo al pase
+		pase_dia.addEventListener('blur', mostrarDias);
+		pase_completo.addEventListener('blur', mostrarDias);
+		pase_dosdias.addEventListener('blur', mostrarDias);
+
+		// Ocultar botones
+		nombre.addEventListener('blur', validarCampos);
+		apellido.addEventListener('blur', validarCampos);
+		email.addEventListener('blur', validarEmail);
 
 		function calcularMontos(event) {
 			event.preventDefault();
@@ -47,12 +63,78 @@
 
 				var totalPagar = (boletosDia*30) + (boletos2Dias*45) + (boletoCompleto*50) + ((cantCamisas*10)*.93) + (cantEtiquetas*2);
 
-				console.log(totalPagar);
+				var listadoProductos = [];
+
+				if (boletosDia >= 1) {
+					listadoProductos.push(boletosDia + ' Pase/s por día');
+				}
+				if (boletos2Dias >= 1) {
+					listadoProductos.push(boletos2Dias + ' Pase/s por dos día');
+				}
+				if (boletoCompleto >= 1) {
+					listadoProductos.push(boletoCompleto + ' Pase/s competos');
+				}
+				if (cantCamisas >= 1) {
+					listadoProductos.push(cantCamisas + ' Camisa/s');
+				}
+				if (cantEtiquetas >= 1) {
+					listadoProductos.push(cantEtiquetas + ' Etiqueta/s');
+				}
+
+				lista_productos.style.display = "block";
+
+				lista_productos.innerHTML = '';
+
+				for (var i = 0; i < listadoProductos.length; i++) {
+					lista_productos.innerHTML += listadoProductos[i] + '<br/>';
+				}
+
+				suma.innerHTML = "$ " + totalPagar.toFixed(2);
 			}
 		}
 
+		function mostrarDias() {
+			var boletosDia = parseInt(pase_dia.value, 10) || 0,
+				boletos2Dias = parseInt(pase_dosdias.value, 10) || 0,
+				boletoCompleto = parseInt(pase_completo.value, 10) || 0;
 
+				var diasElegidos = [];
 
+				if (boletosDia > 0) {
+					diasElegidos.push('#viernes');
+				}
+				if (boletos2Dias > 0) {
+					diasElegidos.push('#viernes' , '#sabado');
+				}
+				if (boletoCompleto > 0) {
+					diasElegidos.push('#viernes' , '#sabado' , '#domingo');
+				}
+				for (var i = 0; i < diasElegidos.length; i++) {
+					document.querySelector(diasElegidos[i]).style.display = 'block';
+				}
+		}
+
+		function validarCampos() {
+			if (this.value == '') {
+				errorDiv.style.display = 'block';
+				alert("Este campo es obligatorio");
+				this.style.border = "1px solid #ff0000";
+			} else {
+				errorDiv.style.display = 'none';
+				this.style.border = "1px solid #a9a9a9";
+			}
+		}
+
+		function validarEmail() {
+			if (this.value.indexOf("@") > -1) {
+				errorDiv.style.display = 'none';
+				this.style.border = "1px solid #a9a9a9";
+			} else {
+				errorDiv.style.display = 'block';
+				alert('Debe tener al menos un "@"');
+				this.style.border = "1px solid #ff0000";
+			}
+		}
 
 
 	}); //DOM CONTENT LOADED
